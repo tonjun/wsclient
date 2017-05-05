7314e97ce0e93518dce674e26f4fa94ce5ee88c6
Tonjun Banaag <tonjun@gmail.com>
2017-05-05 15:57:26 +0800

    Add websocket client implementation

--- wsclient.go
+++ wsclient.go
@@ -28,41 +28,176 @@ Example:
 */
 package wsclient
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
 // WSClient is a WebSocket client
 type WSClient struct {
+	u        string
+	ws       *websocket.Conn
+	send     chan []byte
+	closed   bool
+	closedMu sync.RWMutex
+
+	onOpen    func()
+	onMessage func(data []byte)
+	onClose   func()
+	onError   func(e error)
 }
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+)
 
 // M is a convenient alias for map[string]interface{}
 type M map[string]interface{}
 
 // NewWSClient returns a new instance of WSClient given the WebSocket URL
 func NewWSClient(url string) *WSClient {
-	return &WSClient{}
+	return &WSClient{
+		u:    url,
+		send: make(chan []byte),
+	}
 }
 
 // OnOpen is a callback function when the connection is opened
 func (c *WSClient) OnOpen(fn func()) {
+	c.onOpen = fn
 }
 
 // OnMessage is the callback function when a data is received from the server
 func (c *WSClient) OnMessage(fn func(data []byte)) {
+	c.onMessage = fn
 }
 
-// OnClose is a the callback function when the connection is closed
+// OnClose is the callback function when the connection is closed
 func (c *WSClient) OnClose(fn func()) {
+	c.onClose = fn
+}
+
+// OnError is a callback function for handling errors
+func (c *WSClient) OnError(fn func(err error)) {
+	c.onError = fn
 }
 
 // Connect connects to the WebSocket server
-func (c *WSClient) Connect() error {
-	return nil
+func (c *WSClient) Connect() {
+	go func() {
+		var err error
+		c.ws, _, err = websocket.DefaultDialer.Dial(c.u, nil)
+		if err != nil {
+			fmt.Printf("Connect error: %s", err.Error())
+			if c.onError != nil {
+				c.onError(err)
+			}
+			return
+		}
+		go c.writePump()
+		go c.readPump()
+
+		if c.onOpen != nil {
+			c.onOpen()
+		}
+	}()
 }
 
 // SendJSON sends a JSON encoded message to the server
 func (c *WSClient) SendJSON(j M) error {
+
+	b, err := json.Marshal(j)
+	if err != nil {
+		log.Printf("SendJSON: Marshal error: %s", err.Error())
+		return err
+	}
+	log.Printf("Sending: '%s'", string(b))
+
+	c.send <- b
+
 	return nil
 }
 
 // Close closes the connection from the server
-func (c *WSClient) Close() error {
-	return nil
+func (c *WSClient) Close() {
+	go func() {
+		if c.isClosed() {
+			log.Printf("Close: already closed")
+			return
+		}
+		c.closedMu.Lock()
+		c.closed = true
+		c.closedMu.Unlock()
+		if c.ws != nil {
+			c.ws.Close()
+		}
+		if c.onClose != nil {
+			c.onClose()
+		}
+		close(c.send)
+		log.Printf("Close done")
+	}()
+	return
+}
+
+func (c *WSClient) writePump() {
+	defer func() {
+		c.Close()
+		log.Printf("writePump: done")
+	}()
+	for {
+		select {
+		case mesg, ok := <-c.send:
+			if !ok {
+				return
+			}
+			if err := c.write(websocket.TextMessage, mesg); err != nil {
+				log.Printf("write: error: %s", err.Error())
+				return
+			}
+		}
+	}
+}
+
+func (c *WSClient) readPump() {
+	defer func() {
+		c.Close()
+		log.Printf("readPump: done")
+	}()
+	for {
+		_, message, err := c.ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure) {
+				log.Printf("Read error: %s", err.Error())
+			}
+			break
+		}
+		if c.onMessage != nil {
+			c.onMessage(message)
+		}
+	}
+}
+
+func (c *WSClient) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	if mt != websocket.CloseMessage {
+		if mt == websocket.PingMessage {
+			log.Printf("mt: ping")
+		} else {
+			log.Printf("mt: %d write: '%s'", mt, string(payload))
+		}
+	}
+	return c.ws.WriteMessage(mt, payload)
+}
+
+func (c *WSClient) isClosed() bool {
+	c.closedMu.RLock()
+	defer c.closedMu.RUnlock()
+	return c.closed
 }
--- wsclient_test.go
+++ wsclient_test.go
@@ -1,12 +1,47 @@
 package wsclient
 
 import (
+	"log"
 	"testing"
+	//"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestClient(t *testing.T) {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	ws := NewWSClient("wss://pubsub-local.comms.razerzone.com:7070/ws")
+	done := make(chan bool)
+
+	ws := NewWSClient("ws://localhost:8080")
 	ws.Connect()
+	ws.OnOpen(func() {
+		log.Println("connection opened")
 
+		ws.SendJSON(M{
+			"op": "get-time",
+		})
+
+		ws.OnMessage(func(data []byte) {
+			log.Printf("OnMessage: '%s'", string(data))
+			assert.Equal(t, []byte("{\"op\":\"get-time-response\"}"), data)
+
+			//time.Sleep(10 * time.Millisecond)
+			done <- true
+		})
+
+	})
+
+	log.Printf("waiting to finish")
+	<-done
+
+	ws.OnClose(func() {
+		log.Printf("onClose")
+		done <- true
+	})
+	ws.Close()
+
+	<-done
+
+	//time.Sleep(100 * time.Millisecond)
 }
